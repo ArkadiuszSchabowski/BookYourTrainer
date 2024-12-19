326362dafcaa6847c7126de6e319b93fd6432fd3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2024-12-19 14:20:57 +0100

    feat: add route for reset-password

--- Frontend/BookYourTrainer_Frontend/src/app/app-routing.module.ts
+++ Frontend/BookYourTrainer_Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './logout-components/login/login.component';
 import { MainPageComponent } from './logout-components/main-page/main-page.component';
 import { RegisterComponent } from './logout-components/register/register.component';
+import { ResetPasswordComponent } from './logout-components/reset-password/reset-password.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
   {path: '**', redirectTo: ''}
 ];
 
